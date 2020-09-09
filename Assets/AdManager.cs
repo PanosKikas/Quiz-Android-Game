a362f952e931458a03c5f43351f256beb17aa878
KickAssGr <dai16115@uom.edu.gr>
2020-09-09 22:01:17 +0300

    Revert

---
+++ Assets/AdManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EasyMobile;
+
+public class AdManager : MonoBehaviour
+{
+    public static AdManager Instance;
+
+    #region Singleton
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+    #endregion
+
+    // Subscribe to the event
+    void OnEnable()
+    {
+        Advertising.InterstitialAdCompleted += InterstitialAdCompletedHandler;
+        
+    }
+
+    IEnumerator WaitForAd()
+    {
+        if (!Advertising.IsInterstitialAdReady())
+        {
+            Debug.Log("Nada");
+            yield return null;
+        }
+        else
+        {
+            Advertising.ShowInterstitialAd();
+        }
+    }
+
+    private void Update()
+    { 
+          StartCoroutine(WaitForAd());        
+    }
+
+    // The event handler
+    void InterstitialAdCompletedHandler(InterstitialAdNetwork network, AdPlacement location)
+    {
+        Debug.Log("Interstitial ad has been closed.");
+    }
+    // Unsubscribe
+    void OnDisable()
+    {
+        Advertising.InterstitialAdCompleted -= InterstitialAdCompletedHandler;
+    }
+}
---
+++ Assets/LoadScreenShowAd.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EasyMobile;
+
+public class LoadScreenShowAd : MonoBehaviour
+{
+    private void Start()
+    {
+        // Check if interstitial ad is ready
+        bool isReady = Advertising.IsInterstitialAdReady();
+         
+        // Show it if it's ready
+        if (isReady)
+        {
+            Debug.Log("Show");
+            Advertising.ShowInterstitialAd();
+        }
+    }
+}
