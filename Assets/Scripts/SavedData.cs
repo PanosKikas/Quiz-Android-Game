197961e88a5a54e1807fe0147ef6337e80cd7347
KickAssGr <dai16115@uom.edu.gr>
2020-09-11 11:17:29 +0300

    Imported Google Play Games Plugin

---
+++ Assets/Scripts/SavedData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SavedData : ScriptableObject
+{
+    public string PlayerName;
+    public int HighScore;
+}
